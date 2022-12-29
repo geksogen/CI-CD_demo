bc210a72248087aab154d950df783f9a7123877e
Andrey.Torshin <dchorch@gmail.com>
2022-12-29 21:44:02 +0300

    v10

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app.add_route("/metrics", handle_metrics)
 def read_root(credentials: HTTPBasicCredentials = Depends(get_current_username)):
     return {"Hello from OTUS!!)"}
 
-@app.get("/check")
+@app.get("/test")
 def hello():
     return "Hello World"
 
